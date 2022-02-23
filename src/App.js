a25e096d87bb827bc4963cab7c73a90ee5c5c4e7
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-23 15:54:43 -0300

    show Table Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
 import SWProvider from './context/SWProvider';
 
 function App() {
   return (
     <SWProvider>
       <header>Olá</header>
+      <Table />
     </SWProvider>
   );
 }
