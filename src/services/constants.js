c7b75121a7b86a67f7e3578a17c777d2fe776dc5
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-23 15:34:48 -0300

    URL and options

---
+++ src/services/constants.js
@@ -0,0 +1,8 @@
+export const URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
+export const OPTIONS = [
+  'population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water',
+];
