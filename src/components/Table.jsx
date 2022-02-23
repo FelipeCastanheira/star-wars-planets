cd38538518d8bdd2e7aa00937b721a572d06a583
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-23 15:54:57 -0300

    table component

---
+++ src/components/Table.jsx
@@ -0,0 +1,63 @@
+import React, { useContext } from 'react';
+import SWContext from '../context/SWContext';
+
+function Table() {
+  const { data } = useContext(SWContext);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        { data.map((planet) => {
+          const { name,
+            climate,
+            created,
+            diameter,
+            edited,
+            films,
+            gravity,
+            orbital_period: orbitalPeriod,
+            population,
+            rotation_period: rotationPeriod,
+            surface_water: surfaceWater,
+            terrain,
+            url,
+          } = planet;
+          return (
+            <tr key={ name }>
+              <td>{ name }</td>
+              <td>{ rotationPeriod }</td>
+              <td>{ orbitalPeriod }</td>
+              <td>{ diameter }</td>
+              <td>{ climate }</td>
+              <td>{ gravity }</td>
+              <td>{ terrain }</td>
+              <td>{ surfaceWater }</td>
+              <td>{ population }</td>
+              <td>{ films }</td>
+              <td>{ created }</td>
+              <td>{ edited }</td>
+              <td>{ url }</td>
+            </tr>);
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
