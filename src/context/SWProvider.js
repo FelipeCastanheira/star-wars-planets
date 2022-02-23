030844578efd0610f9d1b9a40e4430415ec85bf3
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-23 16:54:35 -0300

    update context

--- src/context/SWProvider.js
+++ src/context/SWProvider.js
@@ -6,11 +6,23 @@ import SWContext from './SWContext';
 function SWProvider({ children }) {
   const [data, setData] = useState([]);
   const [filterByName, setFilterByName] = useState({ name: '' });
+  const [filterByNumericValues, setFilterByNumericValues] = useState([]);
+  const [column, setColumn] = useState('');
+  const [comparison, setComparison] = useState('maior que');
+  const [value, setValue] = useState('0');
 
   const globalState = {
     data,
     filterByName,
     setFilterByName,
+    filterByNumericValues,
+    setFilterByNumericValues,
+    column,
+    setColumn,
+    comparison,
+    setComparison,
+    value,
+    setValue,
   };
 
   useEffect(() => {
