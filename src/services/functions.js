c5cf6dbd1b16642508582442a8c275d2ea412c32
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-23 21:47:53 -0300

    sort function

--- src/services/functions.js
+++ src/services/functions.js
@@ -1,4 +1,4 @@
-import { URL } from './constants';
+import { NEGATIVE, URL } from './constants';
 
 export const isPlanetCorrect = ({ column, comparison, value }, data) => {
   if (comparison === 'maior que') {
@@ -17,4 +17,25 @@ export const getPlanets = async () => {
   } catch (error) {
     console.error(error);
   }
+};
+const sortByName = (a, b) => {
+  if (a > b) {
+    return 1;
+  }
+  if (a < b) {
+    return NEGATIVE;
+  }
+  return 0;
+};
+const sortByNumber = (a, b, sortNumber) => {
+  const firstNum = Number(a);
+  const secondNum = Number(b);
+  return (firstNum - secondNum) * sortNumber;
+};
+export const getOrder = (a, b, { column, sort }) => {
+  if (column !== 'name') {
+    const sortNumber = (sort === 'ASC') ? 1 : NEGATIVE;
+    return sortByNumber(a[column], b[column], sortNumber);
+  }
+  return sortByName(a.name, b.name);
 };
