24b40fc0b9e1c0b2f91b4756d201c6250c242d2c
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2022-02-24 15:49:22 -0300

    table style

--- src/components/FilterList.jsx
+++ src/components/FilterList.jsx
@@ -13,26 +13,33 @@ function FilterList() {
   };
 
   return (
-    <section>
-      { filterByNumericValues.map(({ column, comparison, value }) => (
-        <div key={ column } data-testid="filter">
-          <span>{`${column} ${comparison} ${value}`}</span>
+    <section className="flex filter-list">
+      <div className="container">
+        { filterByNumericValues.map(({ column, comparison, value }) => (
+          <div key={ column } className="filter" data-testid="filter">
+            <span>{`${column} ${comparison} ${value}`}</span>
+            <button
+              onClick={ () => removeFilter(column) }
+              type="button"
+            >
+              X
+            </button>
+          </div>
+        ))}
+      </div>
+      { filterByNumericValues.length
+        ? (
           <button
-            onClick={ () => removeFilter(column) }
+            data-testid="button-remove-filters"
             type="button"
+            className="remove-all"
+            disabled={ !filterByNumericValues.length }
+            onClick={ () => setFilterByNumericValues([]) }
           >
-            X
+            Remover Filtros
           </button>
-        </div>
-      ))}
-      <button
-        data-testid="button-remove-filters"
-        type="button"
-        disabled={ !filterByNumericValues.length }
-        onClick={ () => setFilterByNumericValues([]) }
-      >
-        Remover Filtros
-      </button>
+        )
+        : ''}
     </section>
   );
 }
